0cbbd1ee654d72cd0d9ddd699a0fb2c38ac76257
kyle devine <truedarkrei9n@hotmail.com>
2016-01-04 21:48:46 -0700

    Made Windows entry part of a class to handle stuff easier
    Signed-off-by: kyle devine <truedarkrei9n@hotmail.com>

--- ThomasTheTankEngine/ThomasTheTankEngine.cpp
+++ ThomasTheTankEngine/ThomasTheTankEngine.cpp
@@ -5,6 +5,8 @@
 #include "ThomasTheTankEngine.h"
 #include "DX12Render.h"
 
+using namespace WindowsApplication;
+
 #define MAX_LOADSTRING 100
 
 // Global Variables:
@@ -12,53 +14,56 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	CWindow Window;
+	Window.TrueMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+}
+int CWindow::TrueMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_THOMASTHETANKENGINE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_THOMASTHETANKENGINE, szWindowClass, MAX_LOADSTRING);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
 
-  //  HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_THOMASTHETANKENGINE));
+	MyRegisterClass(hInstance);
 
-    MSG msg;
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
+	//  HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_THOMASTHETANKENGINE));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
 
 		TranslateMessage(&msg);
-        DispatchMessage(&msg);
-        
-    }
+		DispatchMessage(&msg);
 
-    return (int) msg.wParam;
+	}
+
+	return (int)msg.wParam;
 }
 //
 //  FUNCTION: MyRegisterClass()
 //
 //  PURPOSE: Registers the window class.
 //
-ATOM MyRegisterClass(HINSTANCE hInstance)
+ATOM CWindow::MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
 
@@ -89,7 +94,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+BOOL CWindow::InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
@@ -102,7 +107,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    }
    //Modified to initialize DirectX as well before showing the window
    //TODO: Fix this stupid shit. Possibly incorporate WinMain/WndProc/etc into a class and have contain an object of DX12Render.
-   DirectX12Render::DirectX12Render Render;
+
    Render.Initialize();
 
    ShowWindow(hWnd, nCmdShow);
@@ -121,7 +126,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - post a quit message and return
 //
 //
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK CWindow::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
--- ThomasTheTankEngine/ThomasTheTankEngine.h
+++ ThomasTheTankEngine/ThomasTheTankEngine.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #include "resource.h"
+
+namespace WindowsApplication
+{
+	class CWindow
+	{
+	public:
+		int APIENTRY TrueMain(_In_ HINSTANCE hInstance,
+			_In_opt_ HINSTANCE hPrevInstance,
+			_In_ LPWSTR    lpCmdLine,
+			_In_ int       nCmdShow);
+		ATOM MyRegisterClass(HINSTANCE hInstance);
+		BOOL InitInstance(HINSTANCE hInstance, int nCmdShow);
+		static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+	private:
+		HWND hWNd;
+		DirectX12Render::DirectX12Render Render;
+	};
+}
